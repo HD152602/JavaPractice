604d345cac291a5049b19de8103242114f58573b
rkdal <rkdal@LAPTOP-DDR2P97I>
2017-03-23 21:27:42 +0900

    generate getters/setters를 사용하지 않고 다시 짠 코드임

--- JavaPractice/src/org/dimigo/oop/Car.java
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -18,63 +18,33 @@ package org.dimigo.oop;
 public class Car {
 	private String company, model, color;
 	private int maxSpeed, price;
-	/**
-	 * @return the company
-	 */
 	public String getCompany() {
 		return company;
 	}
-	/**
-	 * @param company the company to set
-	 */
 	public void setCompany(String newCompany) {
 		company = newCompany;
 	}
-	/**
-	 * @return the model
-	 */
 	public String getModel() {
 		return model;
 	}
-	/**
-	 * @param model the model to set
-	 */
 	public void setModel(String newModel) {
 		model = newModel;
 	}
-	/**
-	 * @return the color
-	 */
 	public String getColor() {
 		return color;
 	}
-	/**
-	 * @param color the color to set
-	 */
 	public void setColor(String newColor) {
 		color = newColor;
 	}
-	/**
-	 * @return the maxSpeed
-	 */
 	public int getMaxSpeed() {
 		return maxSpeed;
 	}
-	/**
-	 * @param maxSpeed the maxSpeed to set
-	 */
 	public void setMaxSpeed(int newMaxSpeed) {
 		maxSpeed = newMaxSpeed;
 	}
-	/**
-	 * @return the price
-	 */
 	public int getPrice() {
 		return price;
 	}
-	/**
-	 * @param price the price to set
-	 */
 	public void setPrice(int newPrice) {
 		price = newPrice;
 	}
