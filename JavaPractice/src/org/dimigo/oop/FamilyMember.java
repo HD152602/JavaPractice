c82aaee4b4db4444bcb01014bb809dd58793b163
HD152602 <xowns3131@gmail.com>
2017-04-18 10:29:07 +0900

    Add files via upload

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * <pre>
+ * 
+ *   |_ FamilyMember
+ *
+ * 1.개요 : 
+ * 2.작성일 : 2017. 4. 18.
+ * </pre>
+ *
+ * @author : 강태준
+ * @version : 1.0
+ */
+public class FamilyMember {
+	private static int memberCnt;
+	private String memberName;
+	public FamilyMember(String memberName) {
+		this.memberName = memberName;
+		memberCnt++;
+	}
+	public static void printMemberCnt()  {
+		System.out.println("가족 총 인원 수 : "+memberCnt+"명");
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+}
