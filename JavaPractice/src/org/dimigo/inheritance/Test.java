e72fe5248ddcdbb8b95162803204219a09d8b5a7
rkdal <rkdal@LAPTOP-DDR2P97I>
2017-04-24 12:49:34 +0900

    for test

---
+++ JavaPractice/src/org/dimigo/inheritance/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ Test
+ *
+ * 1.개요 : 
+ * 2.작성일 : 2017. 4. 24.
+ * </pre>
+ *
+ * @author : 강태준
+ * @version : 1.0
+ */
+public class Test {
+public static void main(String[] args) {
+	System.out.println("TEST");
+}
+}
