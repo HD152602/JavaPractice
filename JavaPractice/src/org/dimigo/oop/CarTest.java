15cfab940c0caee37333554dde03bdd0a3c5aead
HD152602 <xowns3131@gmail.com>
2017-04-24 12:28:33 +0900

    Add files via upload

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -1,15 +1,14 @@
 /**
  * 
  */
-package org.dimigo.oop;
 
 /**
  * <pre>
- * org.dimigo.oop
+ * 
  *   |_ CarTest
  *
  * 1.개요 : 
- * 2.작성일 : 2017. 3. 23.
+ * 2.작성일 : 2017. 4. 17.
  * </pre>
  *
  * @author : 강태준
@@ -17,30 +16,14 @@ package org.dimigo.oop;
  */
 public class CarTest {
 	public static void main(String[] args) {
-		Car h = new Car();
-		Car k = new Car();
-		Car s = new Car();
-		h.setColor("검정색");
-		h.setCompany("현대자동차");
-		h.setMaxSpeed(225);
-		h.setModel("제네시스");
-		h.setPrice(50000000);
-		
-		k.setColor("흰색");
-		k.setCompany("기아자동차");
-		k.setMaxSpeed(246);
-		k.setModel("K7");
-		k.setPrice(40000000);
-		
-		s.setColor("회색");
-		s.setCompany("삼성자동차");
-		s.setMaxSpeed(200);
-		s.setModel("SM7");
-		s.setPrice(28000000);
-		System.out.println("<<자동차 목록>>");
-		System.out.println("제조사명 : "+h.getCompany()+"\n모델명 : "+h.getModel()+"\n색상 : "+h.getColor()+"\n최대속도 : "+h.getMaxSpeed()+"km\n가격 : "+String.format("%,d", h.getPrice())+"원\n");
-		System.out.println("제조사명 : "+k.getCompany()+"\n모델명 : "+k.getModel()+"\n색상 : "+k.getColor()+"\n최대속도 : "+k.getMaxSpeed()+"km\n가격 : "+String.format("%,d", k.getPrice())+"원\n");
-		System.out.println("제조사명 : "+s.getCompany()+"\n모델명 : "+s.getModel()+"\n색상 : "+s.getColor()+"\n최대속도 : "+s.getMaxSpeed()+"km\n가격 : "+String.format("%,d", s.getPrice())+"원\n");
-
+		Car.setCompnay("현대");
+		Car[] cars = {
+			new Car("1"),new Car("2")
+		};
+		for(Car car : cars) {
+			System.out.println(car);
+		}
+		System.out.println(String.format("%,d", 100000));
+		System.out.println(Integer.parseInt("10000"));
 	}
 }
